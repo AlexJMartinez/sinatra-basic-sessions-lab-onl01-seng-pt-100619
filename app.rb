0d0e4e5e443c220279d1583f13cf6ea957c6523e
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-29 03:03:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,20 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   get '/' do
     erb:index
   end
 
-configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+
 
 get '/hey' do
   @session = session
 end
 
 post '/checkout'
+@session <<
 
 end
 
